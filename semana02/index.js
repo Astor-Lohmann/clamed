38276a7f65cf67c5b17177a9d7d5578052e0a2ec
astor-lohmann <astor.lohmann@gmail.com>
2022-07-06 19:13:25 -0300

    main

---
+++ semana02/index.js
@@ -0,0 +1,14 @@
+// var idade = window.prompt("Digite sua idade");
+
+// document.write(idade)]
+
+// var isConfirm = window.confirm();
+// document.write("<h1>" + isConfirm + "</h1>");
+// document.write(typeof isConfirm);
+
+
+var nome = window.prompt("Digite seu nome")
+var sobrenome = window.prompt("Digite seu sobrenome")
+
+console.log(nome + " " + sobrenome)
+
