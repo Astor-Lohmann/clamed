fab11d9c65df596c4d673f975dd5340105bc7374
astor-lohmann <astor.lohmann@gmail.com>
2022-07-31 22:10:51 -0300

    semana05

--- Semana05/index.js
+++ Semana05/index.js
@@ -1,18 +1,100 @@
 
-const cidades = [
-    { nome: 'Patos de Minas', populacao: 153585 },
-    { nome: 'Lages', populacao: 157349 },
-    { nome: 'Ibiúna', populacao: 79479 },
-    { nome: 'Maringá', populacao: 403063 },
-    { nome: 'Curitiba', populacao: 1963726 },
-    { nome: 'Florianópolis', populacao: 508826 },
-    { nome: 'Pato Branco', populacao: 84779 },
-];
+// // Exercicio 02
+// const array = [2, 5, 7, 1 , 8, 12, 15, 26, 17, 19, 54, 61, 67]
+// verificaPar = (num) => {
+//     if (num % 2 == 0){
+//         resultado = true
+//     }else if (num % 2 == 1){
+//         resultado = false
+//     }
+// };
+
+// verificaImpar = (num) => {
+//     if (num % 2 == 0){
+//         resultado = false
+//     }else if (num % 2 == 1){
+//         resultado = true
+//     }
+// }
+
+// filtro = (array, verifica) => {
+//     let arrayFiltrado = [];
+//     array.forEach(element => {
+//         verifica(element)
+//         if (verifica == true){
+//             array.push(element)
+//         }
+        
+//     });
+//     console.log(arrayFiltrado)
+// }
+
+// filtro(array, verificaPar)
+
+// array = [3, 2 , 5, 6, 7, 90, 23]
+
+// const calcularMedia = (array) => {
+//     let soma = 0;
+//     for( let i = 0; i < array.length; i++ ){
+//     soma += array[i];
+// }
+
+// let media = soma/array.length;
+// console.log(media)
+// }
+
+// calcularMedia(array)
+
+// array = [3, 2 ,-5, 6, -7, -9, 10 ]
+
+// const separarPositivo = (array) => {
+//     let positivo = array.filter(function(v) {
+//         return v < 0;
+// }
+// )
+// console.log(positivo)
+// }
+// array = [3, 4, -6, 0, -8, -1, 3, -6, 1, -8, -6, -1];
+// const separarPositivo = (array) => {
+//       pos = array.filter(function(v) {
+//         return v > 0;
+//       })
+//       console.log(pos)
+// }
+// separarPositivo(array)
+
+//Exercicio 03
+
+// Utilize o if ternário para construir uma função 
+// que recebe o número de dias e 
+// retorna se o número pertence a primeira 
+// ou a segunda quinzena
+
+// const verificaDia = dia => dia <= 15 ? console.log("primeira quinzena") : console.log("segunda quinzena")
+
+// verificaDia(10)
 
 
-function filtro (cidades) {
-    return cidades.populacao >= 200000
-}
+//Exercicio 04
 
-const grandesCidades = cidades.filter(filtro);
-console.log(grandesCidades)
+// let array = [5, 2, 67, 37, 85, 19, 10];
+
+// const localizaMaior = (array) =>{
+//     maior = 0;
+//     array.forEach((element) => {
+//         if (maior < element) {
+//           maior = element;
+//         }
+//       });
+//       console.log(maior);
+      
+// }
+// localizaMaior(array)
+
+//Exercicio 06
+
+// let array = [19,50,2,1,99]
+// const ordenaArray = (array=[]) => {
+//     return array.sort((a,b)=>a-b)
+// }
+// console.log(ordenaArray(array))
